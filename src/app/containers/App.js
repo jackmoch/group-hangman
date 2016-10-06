3218e4a521f9852e43a9500a1f1fe9f8f72495fa
Jack Mocherman <mochermanjack@gmail.com>
2016-10-06 13:43:09 -0500

    re added lost content in app.js

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+  
+class App extends React.Component {
+    render() {
+      return (
+        <div>
+        <h1>Hello from {this.props.user.hello}</h1>
+        <button onClick={() => this.props.setName('hello')}>Hello</button>
+        </div>
+      )
+    }
+  }
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello from React</h1>
-      </div>
-    )
+const mapStateToProps = state => {
+  return {
+    user: state.reducer
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setName: name => {
+      dispatch({
+        type: 'HELLO',
+        payload: name
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
