936e45a41f496414014be51f9ef611e00b9995cf
Thomas Swann <tkswann@mac.com>
2016-10-06 11:53:09 -0500

    last commit before pivot to traditional react/redux setup

--- views/game.jsx
+++ views/game.jsx
@@ -2,12 +2,17 @@ const React = require('react')
 const DefaultLayout = require('./layouts/master')
 
 const GameComponent = React.createClass({
+	handleSubmit: function(e) {
+			console.log('handleSubmit', e)
+		},
+
 	render: function() {
 		return(
 			<DefaultLayout name={this.props.name}>
 				<div>
 					<h1>XXXXX</h1>
-					<input/>
+					<input type='text'  value={this.props.name} onChange={this.handleSubmit}/>
+					<button onClick={() => console.log('hello')}>Submit</button>
 				</div>
 			</DefaultLayout>
 		)
--- views/index.jsx
+++ views/index.jsx
@@ -8,7 +8,7 @@ const Index = React.createClass({
     return (
       <DefaultLayout name={this.props.name}>
         <div name={this.props.name}>
-          <h1>This is built using React {this.props.name}</h1>
+          <h1>This is built using {this.props.name}</h1>
         </div>
       </DefaultLayout>
     )
