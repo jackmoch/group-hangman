d7c51735a3aef585a86ae2c28016b8a86f6ecafd
Thomas Swann <tkswann@mac.com>
2016-10-06 13:39:15 -0500

    redux connected to inital layout

--- src/app/index.js
+++ src/app/index.js
@@ -3,4 +3,35 @@ import {render} from "react-dom";
 
 import App from "./containers/App"
 
-render(<App />, window.document.getElementById('app'));
+import { createStore, combineReducers, applyMiddleware } from'redux'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+
+const reducer = (state = {
+  hello: 'goobye'
+}, action) => {
+
+  switch(action.type) {
+    case "HELLO":
+      state = {
+        ...state,
+        hello: action.payload
+      }
+      return state
+  }
+  return state
+}
+
+const store = createStore(
+  combineReducers({reducer}),
+  {},
+  applyMiddleware(logger())
+)
+
+store.subscribe(() => {
+
+})
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, window.document.getElementById('app'));
