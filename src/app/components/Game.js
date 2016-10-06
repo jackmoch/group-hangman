18ebbad40ea4e946bbc1f83ef3bf8a8889a12792
Thomas Swann <tkswann@mac.com>
2016-10-06 17:37:49 -0500

    refactored Game to use destructuring, and moved helper functions out of render method

--- src/app/components/Game.js
+++ src/app/components/Game.js
@@ -2,34 +2,40 @@ import React from 'react'
 
 export default class Game extends React.Component {
   render() {
+    const {
+      props, populateArray,
+      testLetter, clearInputs } = this
+    const { user, guessLetter } = props
+
     let letter
-    const {user} = this.props
-    const handleChange = function(e) {
-       letter = e.target.value
-    }
-    const populateArray = function (){
-      user.word.split('').forEach(v => user.guessArray.push(' _ '))
-    }
-    if(user.guessArray.length === 0) {
-      populateArray()
-    }
-    const testLetter = function() {
-      user.word.split('').forEach((v,i) => {
-        if(user.letter.toLowerCase() === v.toLowerCase()) {
-          user.guessArray.splice(i, 1, ` ${v} `)
-        }
-      })
-    }()
-    const clearInputs = function(e) {
-      e.target.value = ''
-    }
+    const handleChange = ({target}) => {
+       letter = target.value.length === 1
+        ? target.value
+        : target.value.slice(0,1)
+     }
+    ;(user.guessArray.length === 0) ? populateArray(user) : null
+    testLetter(user)
+
     return (
       <div>
         <h3>Turns remaining: {user.turns}</h3>
         {user.guessArray.map((v = _,i) => <span key={i}>{v}</span>)}
         <input type='text' onChange={handleChange} onBlur={clearInputs} placeholder='your letter here'/>
-        <button onClick={() => this.props.guessLetter(letter)}>Placeholder</button>
+        <button onClick={() => guessLetter(letter)}>Placeholder</button>
       </div>
     )
+  }
+  clearInputs(e) {
+    e.target.value = ''
+  }
+  testLetter(user) {
+    user.word.split('').forEach((v,i) => {
+      if(user.letter.toLowerCase() === v.toLowerCase()) {
+        user.guessArray.splice(i, 1, ` ${v} `)
+      }
+    })
+  }
+  populateArray(user) {
+    user.word.split('').forEach(v => user.guessArray.push(' _ '))
   }
 }
