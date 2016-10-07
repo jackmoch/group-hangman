cd8ffb0a38d4b5150cea841767a22a5f3ed0a9b7
Jack Mocherman <mochermanjack@gmail.com>
2016-10-07 14:07:51 -0500

    connected to mongoose in server

--- server.js
+++ server.js
@@ -6,12 +6,22 @@ const server = createServer()
 const socket_io = require('socket.io')
 
 server.listen(PORT)
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/hangman'
 
 const io = socket_io()
 io.attach(server)
 
+const mongoose = require('mongoose')
+mongoose.Promise = Promise
+
+mongoose.connect(MONGODB_URL, () => {
+	console.log('Mongod Connected')
+}) 
 
 io.on('connection', socket => {
+
+	console.log(socket.handshake.headers.referer.split('/').slice(-1)[0])
+
   console.log(`Socket connected: ${socket.id}`)
   socket.on('action', action => {
     console.log('ACTION',action)
